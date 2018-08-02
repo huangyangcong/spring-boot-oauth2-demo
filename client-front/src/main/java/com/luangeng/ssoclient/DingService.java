762262030bc06487af67a0f205b92588c62c07aa
luangeng <1057935399@qq.com>
2018-08-02 18:21:11 +0800

    mod

---
+++ client-front/src/main/java/com/luangeng/ssoclient/DingService.java
@@ -0,0 +1,102 @@
+package com.luangeng.ssoclient;
+
+import lombok.Data;
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+//https://blog.csdn.net/biboheart/article/details/80666700
+@Service
+public class DingService {
+
+    private String ding_url = "https://oapi.dingtalk.com/connect/oauth2/sns_authorize?";
+    private String redirect_url = "https://www.baidu.com/";
+    private String appid = "dingoa1ft83dapn4jhlxnn";
+    private String appSecret = "vXwQ-JarhtSQmILQdh_ONSf0egP1BWnvsv63Lo3ISijtv3AVUZO66jqZq3fk_n6U";
+
+    private String tmp_token_url = "https://oapi.dingtalk.com/sns/gettoken?appid=" + appid + "&appsecret=" + appSecret;
+    private String per_token_url = "https://oapi.dingtalk.com/sns/get_persistent_code?access_token="; //post
+    private String sns_token_url = "https://oapi.dingtalk.com/sns/get_sns_token?access_token=";      //post
+    private String user_info_url = "https://oapi.dingtalk.com/sns/getuserinfo?sns_token=";
+
+    private RestTemplate template = new RestTemplate();
+
+    public TmpToken getTmpToken() {
+        ResponseEntity<TmpToken> tmpToken = template.getForEntity(tmp_token_url, TmpToken.class);
+        if (tmpToken.getStatusCode().equals(HttpStatus.OK)) {
+            return tmpToken.getBody();
+        }
+        return null;
+    }
+
+    public PerToken getPertoken(String code) {
+        TmpToken tmpToken = getTmpToken();
+        String token = tmpToken.getAccess_token();
+        String param = "{'tmp_auth_code':'" + code + "'}";
+        HttpHeaders headers = new HttpHeaders();
+        MediaType type = MediaType.parseMediaType("application/json; charset=UTF-8");
+        headers.setContentType(type);
+        HttpEntity<String> requestEntity = new HttpEntity<String>(param, headers);
+        PerToken perToken = template.postForObject(per_token_url + token, requestEntity, PerToken.class);
+        perToken.setTmpToken(token);
+        return perToken;
+    }
+
+    public SnsToken getSnsToken(String code) {
+        PerToken perToken = getPertoken(code);
+        String param = "{'openid': '" + perToken.getOpenid() + "','persistent_code': '" + perToken.getPersistent_code() + "'}";
+        HttpHeaders headers = new HttpHeaders();
+        MediaType type = MediaType.parseMediaType("application/json; charset=UTF-8");
+        headers.setContentType(type);
+        HttpEntity<String> requestEntity = new HttpEntity<String>(param, headers);
+        SnsToken s = template.postForObject(sns_token_url + perToken.getTmpToken(), requestEntity, SnsToken.class);
+        return s;
+    }
+
+    public UserInfo getuserInfo(String code) {
+        SnsToken snsToken = getSnsToken(code);
+        ResponseEntity<ResuserInfo> tmpToken = template.getForEntity(user_info_url + snsToken.getSns_token(), ResuserInfo.class);
+        if (tmpToken.getStatusCode().equals(HttpStatus.OK)) {
+            return tmpToken.getBody().getUser_info();
+        }
+        return null;
+    }
+
+    @Data
+    private static class DingBase {
+        private long errcode;
+        private String errmsg;
+    }
+
+    @Data
+    private static class TmpToken extends DingBase {
+        private String access_token;
+    }
+
+    @Data
+    private static class PerToken {
+        private String openid;
+        private String unionid;
+        private String persistent_code;
+        private String tmpToken;
+    }
+
+    @Data
+    private static class SnsToken {
+        private String sns_token;
+    }
+
+    @Data
+    public static class UserInfo {
+        private String dingId;
+        private String nick;
+        private String unionid;
+        private String openid;
+    }
+
+    @Data
+    public static class ResuserInfo {
+        private UserInfo user_info;
+    }
+
+}
--- client-front/src/main/resources/static/dingding.js
+++ client-front/src/main/resources/static/dingding.js
@@ -1,42 +1,41 @@
-///API   https://open-doc.dingtalk.com/docs/doc.htm?spm=a219a.7629140.0.0.wcYmTe&treeId=168&articleId=104882&docType=1
-
 const ding_url = "https://oapi.dingtalk.com/connect/oauth2/sns_authorize?";
-const redirect_uri = "https://www.baidu.com/";
+const redirect_url = "https://www.baidu.com/";
 const appid = "dingoa1ft83dapn4jhlxnn";
 const appSecret = "vXwQ-JarhtSQmILQdh_ONSf0egP1BWnvsv63Lo3ISijtv3AVUZO66jqZq3fk_n6U";
 
 const tmp_token_url = "https://oapi.dingtalk.com/sns/gettoken?appid="+appid+"&appsecret="+appSecret;
-const per_token_url = "https://oapi.dingtalk.com/sns/get_persistent_code?access_token=" //post
+const per_token_url = "https://oapi.dingtalk.com/sns/get_persistent_code?access_token="; //post
 const sns_token_url = "https://oapi.dingtalk.com/sns/get_sns_token?access_token=";      //post
 const user_info_url = "https://oapi.dingtalk.com/sns/getuserinfo?sns_token=";
 
-var state = "";
+var state = "qwer";
 var tmp_auth_code;
 var tmp_token;
+var open_id;
+var unionid;
 var per_token;
 var sns_token;
 
 
 function opending(){
-        var obj = DDLogin({
+  var obj = DDLogin({
      id:"login_container",//这里需要你在自己的页面定义一个HTML标签并设置id，例如<div id="login_container"></div>或<span id="login_container"></span>
-     goto: encodeURIComponent(ding_url+"appid="+appid+"&response_type=code&scope=snsapi_login&state="+state+"&redirect_uri="+rediretct),
+     goto: encodeURIComponent(ding_url+"appid="+appid+"&response_type=code&scope=snsapi_login&state="+state+"&redirect_uri="+redirect_url),
      width : "300",
      height: "300"
  });
 }
-
 
 var hanndleMessage = function (event) {
     var origin = event.origin;
     console.log("origin", event.origin);
     //判断是否来自ddLogin扫码事件。
     if( origin == "https://login.dingtalk.com" ) {
-        tmp_auth_code = event.data;
+        var tmp_code = event.data;
         //拿到loginTmpCode后,在这里构造跳转链接进行跳转
-        console.log("loginTmpCode", tmp_auth_code);
-        window.parent.postMessage(tmp_auth_code,'*');
-        window.location.href=ding_url+"appid="+appid+"&response_type=code&scope=snsapi_login&state="+state+"&redirect_uri="+rediretct+"&loginTmpCode="+tmp_auth_code;
+        alert(tmp_code);
+        window.parent.postMessage(tmp_code,'*');
+        window.location.href=ding_url+"appid="+appid+"&response_type=code&scope=snsapi_login&state="+state+"&redirect_uri="+redirect_url+"&loginTmpCode="+tmp_code;
     }
 };
 
@@ -46,8 +45,69 @@ if (typeof window.addEventListener != 'undefined') {
     window.attachEvent('onmessage', hanndleMessage);
 }
 
-function getTmpToken(){
+function getTmpCode(){
+    let params = window.location.search.split("&");
+    tmp_auth_code = params[0].substr(params[0].indexOf("code=")+5);
+    getTmpToken();
+}
 
+function getTmpToken(){
+  $.ajax({
+            url: tmp_token_url,
+            type:"get",
+            data: {"tmp_auth_code": tmp_auth_code},
+            beforeSend(xhr){
+                xhr.setRequestHeader("Access-Control-Allow-Origin", window.location.origin);
+            },
+            success: function (data) {
+                tmp_token = data.access_token;
+                getPerToken();
+            },
+            error:function(a,b,c){
+                console.log(a.responseJSON);
+            }
+         });
+}
+
+function getPerToken(){
+    $.ajax({
+          url: per_token_url+tmp_token,
+          type:"post",
+          data: {"tmp_auth_code": tmp_auth_code},
+          contentType: 'application/json',
+          success: function (data) {
+            per_token = data.persistent_code;
+            open_id = data.openid;
+            unionid = data.unionid;
+            getSnsToken();
+          },
+          error:function(a,b,c){
+           console.log(a.responseJSON);
+          }
+       });
+}
+
+function getSnsToken(){
+    $.ajax({
+         url: sns_token_url+per_token,
+         type:"post",
+         data: {"openid": open_id,
+                "persistent_code": per_token},
+         contentType: 'application/json',
+         success: function (data) {
+            sns_token = data.sns_token;
+            getUserInfo();
+         },
+         error:function(a,b,c) {
+          console.log(a.responseJSON);
+         }
+      });
+}
+
+function getUserInfo(){
+     $.get(user_info_url+sns_token, function(data) {
+          console.log(data);
+     });
 }
 
 
