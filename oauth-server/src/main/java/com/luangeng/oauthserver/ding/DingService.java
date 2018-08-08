6bec3e0ce1ec08fc81a682e9481b509f5bfb72e6
luangeng <1057935399@qq.com>
2018-08-08 11:18:33 +0800

    mod dingding

--- oauth-server/src/main/java/com/luangeng/oauthserver/ding/DingService.java
+++ oauth-server/src/main/java/com/luangeng/oauthserver/ding/DingService.java
@@ -1,51 +1,78 @@
 package com.luangeng.oauthserver.ding;
 
+import com.luangeng.oauthserver.exception.DingServiceException;
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 /**
  * 钉钉扫码登录验证服务
+ *
  */
+@Slf4j
 @Service
 public class DingService {
 
-    private static final int TIMEOUT = 4000;
+    private static final int DING_OK = 0;
+    private static final int RETRY_COUNT = 2;
+    private static final int RETRY_DELAY = 1000 * 60 * 4;
+    private static final int REQUEST_TIMEOUT = 4000;
+    private static final int TOKEN_REFRESH_FIXED_RATE = 1000 * 60 * 110;
+    private static final String JSON_TYPE = "application/json; charset=UTF-8";
+    private static final String per_token_url = "https://oapi.dingtalk.com/sns/get_persistent_code?access_token=";
+    private static final String sns_token_url = "https://oapi.dingtalk.com/sns/get_sns_token?access_token=";
+    private static final String user_info_url = "https://oapi.dingtalk.com/sns/getuserinfo?sns_token=";
 
-    private String appid = "dingoa1ft83dapn4jhlxnn";
-    private String appSecret = "vXwQ-JarhtSQmILQdh_ONSf0egP1BWnvsv63Lo3ISijtv3AVUZO66jqZq3fk_n6U";
+    private int failCount = 0;
 
-    private String tmp_token_url = "https://oapi.dingtalk.com/sns/gettoken?appid=" + appid + "&appsecret=" + appSecret;
-    private String per_token_url = "https://oapi.dingtalk.com/sns/get_persistent_code?access_token=";
-    private String sns_token_url = "https://oapi.dingtalk.com/sns/get_sns_token?access_token=";
-    private String user_info_url = "https://oapi.dingtalk.com/sns/getuserinfo?sns_token=";
+    @Value("${dingding.appId}")
+    private String appId;
+
+    @Value("${dingding.appSecret}")
+    private String appSecret;
 
     private RestTemplate template;
 
+    private AccessToken accessToken = null;
+
     public DingService() {
         SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
-        requestFactory.setConnectTimeout(TIMEOUT);
-        requestFactory.setReadTimeout(TIMEOUT);
+        requestFactory.setConnectTimeout(REQUEST_TIMEOUT);
+        requestFactory.setReadTimeout(REQUEST_TIMEOUT);
         template = new RestTemplate(requestFactory);
     }
 
-    public TmpToken getTmpToken() {
-        ResponseEntity<TmpToken> tmpToken = template.getForEntity(tmp_token_url, TmpToken.class);
-        if (tmpToken.getStatusCode().equals(HttpStatus.OK)) {
-            return tmpToken.getBody();
+    public void getAccessToken() throws InterruptedException {
+        String acc_token_url = "https://oapi.dingtalk.com/sns/gettoken?appid=" + appId + "&appsecret=" + appSecret;
+        ResponseEntity<AccessToken> accessTokenVal = template.getForEntity(acc_token_url, AccessToken.class);
+        if (accessTokenVal.getStatusCode().equals(HttpStatus.OK)) {
+            failCount = 0;
+            AccessToken token = accessTokenVal.getBody();
+            if (token != null && token.getErrcode() == DING_OK) {
+                accessToken = token;
+            } else {
+                log.error("获取钉钉token失败：" + (token == null ? "返回值为空" : token.getErrmsg()));
+            }
+        } else if (failCount < RETRY_COUNT) {
+            failCount++;
+            log.error("获取钉钉token失败：请求未返回成功");
+            Thread.sleep(RETRY_DELAY);
+            getAccessToken();
         }
-        return null;
     }
 
-    public PerToken getPertoken(String code) {
-        TmpToken tmpToken = getTmpToken();
-        checkSuccess(tmpToken);
-        String token = tmpToken.getAccess_token();
+    public PerToken getPerToken(String code) {
+        checkSuccess(accessToken);
+        String token = accessToken.getAccess_token();
         String param = "{'tmp_auth_code':'" + code + "'}";
         HttpHeaders headers = new HttpHeaders();
-        MediaType type = MediaType.parseMediaType("application/json; charset=UTF-8");
+        MediaType type = MediaType.parseMediaType(JSON_TYPE);
         headers.setContentType(type);
         HttpEntity<String> requestEntity = new HttpEntity<String>(param, headers);
         PerToken perToken = template.postForObject(per_token_url + token, requestEntity, PerToken.class);
@@ -54,11 +81,11 @@ public class DingService {
     }
 
     public SnsToken getSnsToken(String code) {
-        PerToken perToken = getPertoken(code);
+        PerToken perToken = getPerToken(code);
         checkSuccess(perToken);
         String param = "{'openid': '" + perToken.getOpenid() + "','persistent_code': '" + perToken.getPersistent_code() + "'}";
         HttpHeaders headers = new HttpHeaders();
-        MediaType type = MediaType.parseMediaType("application/json; charset=UTF-8");
+        MediaType type = MediaType.parseMediaType(JSON_TYPE);
         headers.setContentType(type);
         HttpEntity<String> requestEntity = new HttpEntity<String>(param, headers);
         SnsToken s = template.postForObject(sns_token_url + perToken.getTmpToken(), requestEntity, SnsToken.class);
@@ -68,29 +95,28 @@ public class DingService {
     public UserInfo getUserInfo(String code) {
         SnsToken snsToken = getSnsToken(code);
         checkSuccess(snsToken);
-        ResponseEntity<ResuserInfo> tmpToken = template.getForEntity(user_info_url + snsToken.getSns_token(), ResuserInfo.class);
-        if (tmpToken.getStatusCode().equals(HttpStatus.OK)) {
-            return tmpToken.getBody().getUser_info();
+        ResponseEntity<ResuserInfo> userInfo = template.getForEntity(user_info_url + snsToken.getSns_token(), ResuserInfo.class);
+        if (userInfo.getStatusCode().equals(HttpStatus.OK)) {
+            return userInfo.getBody().getUser_info();
         }
         return null;
     }
 
+    private void checkSuccess(DingBase data) throws DingServiceException {
+        if (data == null) {
+            log.error("调用钉钉接口失败：请求未返回成功");
+            throw new DingServiceException("调用钉钉接口失败：请求未返回成功");
+        }
+        if (data.getErrcode() != DING_OK) {
+            log.error("调用钉钉接口失败：" + data.getErrmsg());
+            throw new DingServiceException("调用钉钉接口失败：" + data.getErrmsg());
+        }
+    }
+
     @Data
     private static class DingBase {
-        private long errcode;
+        private int errcode;
         private String errmsg;
-    }
-
-    @Data
-    private static class TmpToken extends DingBase {
-        private String access_token;
-    }
-
-    private boolean checkSuccess(DingBase data) {
-        if (data != null && data.getErrcode() == 0) {
-            return true;
-        }
-        return false;
     }
 
     @Data
@@ -102,6 +128,11 @@ public class DingService {
     }
 
     @Data
+    private static class AccessToken extends DingBase {
+        private String access_token;
+    }
+
+    @Data
     public static class UserInfo {
         private String dingId;
         private String nick;
@@ -110,13 +141,20 @@ public class DingService {
     }
 
     @Data
-    private static class SnsToken extends DingBase {
-        private String sns_token;
-    }
-
-    @Data
     public static class ResuserInfo extends DingBase {
         private UserInfo user_info;
     }
 
+    @Data
+    private static class SnsToken extends DingBase {
+        private String sns_token;
+    }
+
+    @Component
+    private class TimedTask {
+        @Scheduled(fixedRate = TOKEN_REFRESH_FIXED_RATE)
+        public void fetchAccessToken() throws InterruptedException {
+            getAccessToken();
+        }
+    }
 }
