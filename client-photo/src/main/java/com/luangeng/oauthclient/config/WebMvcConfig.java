368c99df6690d48905c65ad11213bc03d3830c93
luangeng <1057935399@qq.com>
2018-08-31 11:40:09 +0800

    mod

--- client-photo/src/main/java/com/luangeng/oauthclient/config/WebMvcConfig.java
+++ client-photo/src/main/java/com/luangeng/oauthclient/config/WebMvcConfig.java
@@ -20,7 +20,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         //映射url中的public/*到classpath下的public文件夹
-        registry.addResourceHandler("/public/**").addResourceLocations("classpath:/public/");
+        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/");
+        registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/");
+        registry.addResourceHandler("/img/**").addResourceLocations("classpath:/static/img/");
     }
 
     @Override
